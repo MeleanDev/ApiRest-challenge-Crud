337a8cf408f889252b03d607a7eb8dc39fb3bf0a
Luis Acosta <gitmeleandev.phantom644@passinbox.com>
2024-06-22 21:20:05 -0400

    post

--- app/DataBaseCLass.php
+++ app/DataBaseCLass.php
@@ -2,13 +2,26 @@
 
 namespace App;
 
+use App\Models\Estudiantes;
+
 class DataBaseCLass
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+
+    // Estudiantes
+        // Lista
+        public function estudianteLista(){
+            $datos = Estudiantes::all();
+            return $datos;
+        }
+
+        // Crear
+        public function estudianteCrear($datos){
+            $estud = new Estudiantes();
+            $estud->nombre = $datos->nombre;
+            $estud->apellido = $datos->apellido;
+            $estud->cedula = $datos->cedula;
+            $estud->fecha_nacimiento = $datos->fecha_nacimiento;
+            $estud->save();
+        }
+
 }
--- app/Http/Controllers/EstudiantesController.php
+++ app/Http/Controllers/EstudiantesController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\EstudianteClass;
+use App\Http\Requests\EstudiantesRequest;
 
 class EstudiantesController extends Controller
 {
-    //
+    private $EstudianteClass;
+
+    public function __construct(EstudianteClass $EstudianteClass)
+    {
+        $this->EstudianteClass = $EstudianteClass;
+    }
+
+    public function lista(){
+        $datos = $this->EstudianteClass->lista();
+        if ($datos->isEmpty()) {
+            return response()->json(['message' => 'No se encuentran registros'], 200);
+        }
+        return response()->json(['message' => $datos], 200);
+    }
+
+    public function crear(EstudiantesRequest $datos){
+        try {
+            $this->EstudianteClass->crear($datos);
+            return response()->json(['message' => 'Registro exitoso'], 201);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Error en el registro'], 400);
+        }
+        $this->EstudianteClass->crear($datos);
+    }
+
 }
